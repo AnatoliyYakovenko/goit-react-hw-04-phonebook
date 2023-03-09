b972c881a3ab00581a58680a033b4fbfd45557cb
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-03-09 13:23:06 +0200

    copy datas from previos HW

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import css from './ContactList.module.css';
+import PropTypes from 'prop-types';
+
+export const ContactList = ({ contacts, onDeleteContact }) => (
+  <ul className={css.list}>
+    {contacts.map(({ id, name, number }) => (
+      <li key={id} className={css.item}>
+        <p className={css.contact}>
+          {name}
+          <span  className={css.numberSpan}>{number}</span>
+        </p>
+        <button
+          className={css.btn}
+          type="submit"
+          onClick={() => onDeleteContact(id)}
+        >
+          Delete
+        </button>
+      </li>
+    ))}
+  </ul>
+);
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,21 @@
+import css from './Filter.module.css';
+import PropTypes from 'prop-types';
+
+export const Filter = ({ value, onChange }) => (
+  <div className={css.filter}>
+    <label className={css.labelFilter}>
+      Filter
+      <input
+        type="name"
+        value={value}
+        onChange={onChange}
+        className={css.filterInput}
+      />
+    </label>
+  </div>
+);
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
